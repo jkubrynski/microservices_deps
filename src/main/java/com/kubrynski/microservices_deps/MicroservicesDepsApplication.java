bb97e0a0520cbdf3d26a895930373c71770913ab
Jakub Kubrynski <jkubrynski@gmail.com>
2020-09-07 23:44:49 +0200

    Initial version

---
+++ src/main/java/com/kubrynski/microservices_deps/MicroservicesDepsApplication.java
@@ -0,0 +1,13 @@
+package com.kubrynski.microservices_deps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesDepsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesDepsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kubrynski/microservices_deps/MicroservicesDepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kubrynski.microservices_deps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesDepsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
